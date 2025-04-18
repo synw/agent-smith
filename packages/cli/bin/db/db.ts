d3893e8c33af032bf96ad7bbbb2b82b80a2a92bb
synw <synwe@yahoo.com>
2025-04-18 12:46:26 +0200

    Fix in cli initialization
    - Added import for `createDirectoryIfNotExists` to ensure directory exists during db initialization.
    - Updated package version to 0.0.52.

--- packages/cli/bin/db/db.ts
+++ packages/cli/bin/db/db.ts
@@ -1,6 +1,7 @@
 import DatabaseConstructor, { Database } from "better-sqlite3";
 import { schemas } from "./schemas.js";
 import path from "path";
+import { createDirectoryIfNotExists } from "../cmd/sys/dirs.js";
 
 const confDir = path.join(process.env.HOME ?? "~/", ".config/agent-smith/cli");
 const dbPath = path.join(confDir, "config.db");
@@ -8,6 +9,7 @@ let db: Database;
 
 function initDb(isVerbose: boolean, execSchema = false) {
     if (execSchema) {
+        createDirectoryIfNotExists(confDir, true);
         db = new DatabaseConstructor(dbPath, { fileMustExist: false });
         schemas.forEach((s) => {
             db.exec(s);
