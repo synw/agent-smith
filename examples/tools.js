9a157e0053ad3c5fd74ddccef46c80a310fbf2f6
synw <synwe@yahoo.com>
2025-05-01 13:43:28 +0200

    Update tool call examples
    - Added a new traffic tool example in JavaScript to get current road traffic conditions
    - Updated weather tool response in Python to include weather condition information
    - Updated model configurations and parameters in toolsdemo.yml and toolsexample.yml for better performance and functionality
    - Modified the tools.js file to correct function names, update prompts, and improve tool execution flow

--- examples/tools.js
+++ examples/tools.js
@@ -1,28 +1,34 @@
 #!/usr/bin/env node
 import { default as fs } from "fs";
-import { PromptTemplate } from "modprompt";
 import { useAgentBrain } from "@agent-smith/brain";
 //import { LmTaskBuilder } from "@agent-smith/lmtask";
-//import { useAgentBrain } from "../packages/brain/dist/brain.js";
 import { LmTaskBuilder } from "../packages/lmtask/dist/main.js"
+
+/* 
+To use this as a cli task (will use the toolsList param from the toolsexample.yml task):
+
+lm toolsexample "what is the weather in Monaco?"
+
+Otherwise use as an independant lmtask as shown below, providing the functions to call
+*/
 
 // Run an Ollama server
 
-//const model = { name: "granite3.3:2b", ctx: 2048, template: "granite-tools" };
+const model = { name: "granite3.3:2b", ctx: 2048, template: "granite-tools" };
 //const model = { name: "qwen3:4b", ctx: 2048, template: "chatml-tools" };
-const model = { name: "mistral-small3.1:24b", ctx: 2048, template: "mistral-system-tools" };
+//const model = { name: "mistral-small:latest", ctx: 2048, template: "mistral-system-tools" };
 
-//const _prompt = "What are the current weather and trafic conditions in Barcelona?"
-const _prompt = "What is the current weather in Barcelona?"
+const _prompt = "What are the current weather and trafic conditions in Barcelona?"
+//const _prompt = "What is the current weather in Barcelona?"
 
 function get_current_weather(args) {
     console.log("Running the get_current_weather tool with args", args);
-    return '{“temp”: 20.5, “unit”: “C”}'
+    return { "temp": 20.5, "weather": "rain" }
 }
 
-function get_current_trafic(args) {
-    console.log("Running the get_current_trafic tool with args", args);
-    return '{“trafic”: "normal"}'
+function get_current_traffic(args) {
+    console.log("Running the get_current_traffic tool with args", args);
+    return { "trafic": "normal" }
 }
 
 const weatherToolDef = {
@@ -35,15 +41,15 @@ const weatherToolDef = {
     },
     "execute": get_current_weather,
 };
-const traficToolDef = {
-    "name": "get_current_trafic",
-    "description": "Get the current road trafic conditions",
+const trafficToolDef = {
+    "name": "get_current_traffic",
+    "description": "Get the current road traffic conditions",
     "arguments": {
         "location": {
             "description": "The city and state, e.g. San Francisco, CA"
         }
     },
-    "execute": get_current_trafic,
+    "execute": get_current_traffic,
 };
 
 async function main() {
@@ -56,7 +62,7 @@ async function main() {
     const taskBuilder = new LmTaskBuilder(brain);
     // build the task
     const taskSpec = taskBuilder.readFromYaml(ymlTaskDef);
-    taskSpec.tools = [weatherToolDef, traficToolDef];
+    taskSpec.tools = [weatherToolDef, trafficToolDef];
     const task = taskBuilder.init(taskSpec);
     console.log("Running task...", ymlTaskDef);
     // run the tasks
@@ -64,45 +70,14 @@ async function main() {
         model: model,
         debug: true,
     };
-    const template = new PromptTemplate(model.template);
+    //const template = new PromptTemplate(model.template);
     const res = await task.run({
         prompt: _prompt,
     }, conf);
-    const { isToolCall, toolsCall, error } = template.processAnswer(res.text);
-    if (error) {
-        throw new Error(`Error processing tool call answer:\n, ${answer}`);
-    }
-    if (!isToolCall) {
-        return
-    }
-    let toolResponse = {};
-    toolsCall.forEach((tc) => {
-        console.log("Executing tool call:", tc);
-        if (tc.name == "weather") {
-            toolResponse = get_current_weather(tc.arguments)
-        }
-    });
-    console.log("Tools response", toolResponse);
-    //console.log("\nProcessed answer", isToolCall, toolsCall, error);
-    //return
-    template.pushToHistory({
-        user: prompt,
-        assistant: res.text,
-        tool: toolResponse.toString(),
-    });
-    console.log("\n----------- Turn 2 prompt:");
-    const _nextPrompt = template.render();
-    console.log(_nextPrompt);
-    console.log("------------\n");
-    await lm.infer(_nextPrompt, {
-        stream: true,
-        temperature: 0.1,
-        max_tokens: 1024,
-        extra: {
-            raw: true
-        }
-    });
-    console.log()
+    console.log("\n\n----------- Template history:");
+    console.log(res.template.history);
+    //console.log("\n\n----------- Next turn prompt template:");
+    //console.log(res.template.render());
 }
 
 (async () => {
