f282d1f01ee46fb3d88833d0c222186308448a6d
synw <synwe@yahoo.com>
2025-11-14 16:10:59 +0100

    Update doc

--- docsite/src/pages/index.vue
+++ docsite/src/pages/index.vue
@@ -4,53 +4,24 @@
       <div class="w-[400px] h-[300px]">
         <img src="/img/agentsmith.png" alt="" width="400" height="300" />
       </div>
-      <div class="mt-5 flex flex-col w-[620px]">
-        <template v-if="!isConfigModel">
-          <div class="prosed">
-            <h2>Interactive doc config</h2>
-          </div>
-          <div>Agent Smith's documentation has interactive examples. To run them configure
-            an inference backend using a local Llama.cpp, Koboldcpp or Ollama or just the browser.
-          </div>
-          <div class="mt-5">
-            <button v-if="brain.experts.length == 0" class="btn success" @click="isConfigModel = true">Configure an
-              expert</button>
-            <div v-else class="flex flex-col space-y-3">
-              <div>
-                Using a <span class="font-semibold">{{ brain.ex.backend.name }}</span> backend with
-                model <span class="font-semibold">{{ brain.ex.backend.lm.model.name }}</span>
-              </div>
-            </div>
-          </div>
-          <div class="mt-5 flex flex-row space-x-2 items-center">
-            <div>The agent config can be updated later using this icon</div>
-            <div class="txt-light">
-              <prompt-icon class="text-3xl"></prompt-icon>
-            </div>
-            <div>in the topbar</div>
-          </div>
-          <div class="mt-3">This interactive doc works with the <a
-              href="https://huggingface.co/bartowski/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/Qwen2.5-0.5B-Instruct-Q5_K_M.gguf">Qwen
-              2.5 0.5b q5k_m</a> model or another of
-            your choice</div>
-        </template>
-        <template v-else>
-          <div class="prosed">
-            <h2>Agent config</h2>
-          </div>
-          <agent-conf @end="isConfigModel = false"></agent-conf>
-        </template>
+      <div class="mt-12 flex flex-col w-[620px]">
+        <div class="text-2xl">What Agent Smith can do:</div>
+        <ul class="mt-8 list-disc space-y-2">
+          <li><b>Think</b>: use language model servers to perform inference queries</li>
+          <li><b>Work</b>: manage long running workflows with multiple tasks, using tools</li>
+          <li><b>Remember</b>: use semantic memory to store data</li>
+          <li><b>Interact</b>: perform interactions with the user</li>
+        </ul>
       </div>
     </div>
     <div class="ml-8">
-      <render-md :hljs="hljs" :source="side"></render-md>
       <div class="flex flex-col mt-5">
         <div>Quick example:</div>
         <static-code-block :hljs="hljs" :code="code" lang="typescript"
           class="not-prose mt-5 max-w-screen-lg"></static-code-block>
       </div>
       <div>
-        <button class="mt-5 btn" @click="router.push('/the_body/overview')">Next: the body: overview</button>
+        <button class="mt-5 btn" @click="router.push('/terminal_client/install')">Next: terminal client</button>
       </div>
     </div>
   </div>
@@ -58,47 +29,35 @@
 
 <script setup lang="ts">
 import { hljs } from "@/conf";
-import { RenderMd } from "@docdundee/vue";
 import { StaticCodeBlock } from "@docdundee/vue";
-import PromptIcon from "@/widgets/PromptIcon.vue";
-import AgentConf from '@/components/agentconf/AgentConf.vue';
-import { ref } from "vue";
 import { useRouter } from "vue-router";
-import { brain } from "@/agent/agent";
 
-const isConfigModel = ref(false);
 const router = useRouter();
 
-const side = `### What can Agent Smith do?
+const code = `import { Agent } from "@agent-smith/agent";
+import { Lm } from "@locallm/api";
 
-- **Interact**: the agent can perform interactions with the user and get input and feedback
-- **Think**: interact with some language model servers to perform inference queries
-- **Run jobs**: manage long running jobs with multiple tasks
-- **Remember**: use it's transient or semantic memory to store data`;
-
-const code = `const backend = useLmBackend({
-    name: "koboldcpp",
-    localLm: "koboldcpp",
+const lm = new Lm({
+    providerType: "openai",
+    serverUrl: serverUrl,
+    apiKey: apiKey,
     onToken: (t) => process.stdout.write(t),
 });
 
-const ex = useLmExpert({
-    name: "koboldcpp",
-    backend: backend,
-    template: templateName,
-    model: { name: modelName, ctx: 2048 },
-});
-
-const brain = useAgentBrain([backend], [ex]);
-
-await brain.init();
-brain.ex.checkStatus();
-if (brain.ex.state.get().status == "ready") { 
-  const params = { temperature: 0.2 };
-    const res = await brain.think(
-        prompt,
-        params,
-    );
-    console.log(res.stats);
-}`;
+const agent = new Agent(lm);
+await agent.run(_prompt,
+    //inference params
+    {
+        temperature: 0.5,
+        top_k: 40,
+        top_p: 0.95,
+        min_p: 0,
+        max_tokens: 4096,
+        model: { name: "qwen4b" }
+    },
+    // query options
+    {
+        verbose: true,
+        system: "You are a helpful assistant",
+    });`;
 </script>
