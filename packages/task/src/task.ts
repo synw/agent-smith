45b592d023983e3da4214d80ce5d61fb0e673fce
synw <synwe@yahoo.com>
2026-03-30 17:37:47 +0200

    task: Refactor template handling and OpenAI provider logic
    - Removed conditional template handling based on provider type, now always uses templates regardless of provider
    - Simplified the task execution flow by removing the useTemplates flag logic
    - Updated dependency versions from 0.3.3 to 0.4.0 for @agent-smith/agent package
    - Modified console logging to always use template prompt for non-openai providers
    - Moved history handling to always occur when using OpenAI provider

--- packages/task/src/task.ts
+++ packages/task/src/task.ts
@@ -32,7 +32,7 @@ class Task {
         }
         let model = this.def.model;
         let ctx = this.def.ctx;
-        const useTemplates = this.agent.lm.providerType !== "openai";
+        //const useTemplates = this.agent.lm.providerType !== "openai";
         if (conf) {
             if (conf?.model) {
                 model = conf.model;
@@ -86,21 +86,17 @@ class Task {
             }
         }
         applyVariables(this.def, params);
-        if (useTemplates) {
-            this.def.model = model;
-            tpl = formatTaskTemplate(this.def, model?.template ? model.template : undefined);
-            this.def.inferParams = formatInferParams(this.def.inferParams ?? {}, conf ?? {}, tpl);
-            //tpl.replacePrompt(this.def.prompt);
-            if (hasTools) {
-                if (!tpl?.toolsDef) {
-                    throw new Error(`The template ${tpl.name} does not have tools and the task ${this.def.name} specifies some`)
-                }
-                this.def.tools?.forEach((t) => tpl.addTool(t));
-            };
-            //finalPrompt = params.prompt;
-        } else {
-            this.def.inferParams = formatInferParams(this.def.inferParams ?? {}, conf ?? {});
-        }
+        this.def.model = model;
+        tpl = formatTaskTemplate(this.def, model?.template ? model.template : undefined);
+        this.def.inferParams = formatInferParams(this.def.inferParams ?? {}, conf ?? {}, tpl);
+        //tpl.replacePrompt(this.def.prompt);
+        if (hasTools) {
+            if (!tpl?.toolsDef) {
+                throw new Error(`The template ${tpl.name} does not have tools and the task ${this.def.name} specifies some`)
+            }
+            this.def.tools?.forEach((t) => tpl.addTool(t));
+        };
+        //finalPrompt = params.prompt;
         /*console.log("-------------------------");
         console.log("DEF", this.def);
         console.log("-------------------------");*/
@@ -123,7 +119,7 @@ class Task {
                 });
             }
             console.log("-----------", model.name, "- Template:", tpl.name, "- Ctx:", ctx, "-----------");
-            console.log(useTemplates ? tpl.prompt(finalPrompt) : finalPrompt);
+            console.log(this.agent.lm.providerType != 'openai' ? tpl.prompt(finalPrompt) : finalPrompt);
             console.log("----------------------------------------------")
             console.log("Infer params:", this.def.inferParams);
             console.log("----------------------------------------------")
@@ -148,24 +144,19 @@ class Task {
         if (isRoutingAgent) {
             options.isToolsRouter = true
         }
-        if (!useTemplates) {
-            if (this.def.template?.system) {
-                options.system = this.def.template.system;
-            }
-            if (this.def?.shots) {
-                options.history = options?.history ? [...this.def.shots, ...options.history] : this.def.shots;
-            }
-            //console.log("RUN AGENT (TASK) params:", this.def.inferParams);
-            //console.log("RUN AGENT (TASK) options:", options);
-            answer = await this.agent.run(finalPrompt, this.def.inferParams, options);
-        } else {
-            //console.log("RUN AGENT (TASK) params:", this.def.inferParams);
-            //console.log("RUN AGENT (TASK) options:", options);
-            answer = await this.agent.run(finalPrompt, this.def.inferParams, options, tpl);
-            tpl.history = this.agent.history;
-            //console.log("RAW ANSWER", answer);
-            //console.log("\nHISTORY", this.agent.history);
+        if (this.def.template?.system) {
+            options.system = this.def.template.system;
         }
+        if (this.def?.shots) {
+            options.history = options?.history ? [...this.def.shots, ...options.history] : this.def.shots;
+        }
+        //console.log("RUN AGENT (TASK) params:", this.def.inferParams);
+        //console.log("RUN AGENT (TASK) options:", options);
+        answer = await this.agent.run(finalPrompt, this.def.inferParams, options, tpl);
+        if (this.agent.lm.providerType == "openai") {
+            tpl.history = this.agent.history;
+        }
+
         // remove task tools from the agent
         if (hasTools) {
             this.def.tools?.forEach(
